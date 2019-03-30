7654dd39359bdc476ff1b9f34d1f6c0bbd70a5be
yunheur <yunheur@gmail.com>
2019-03-31 01:34:33 +0900

    add/modify middleware

--- ch6/learn-express/app.js
+++ ch6/learn-express/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var flash = require('connect-flash');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,10 +16,20 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser('secret code'));
+app.use(session({
+	resave: false,
+	saveUninitialized: false,
+	secret: 'secret code',
+	cookie: {
+		httpOnly: true,
+		secure: false,
+	}
+}));
+app.use(flash())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
