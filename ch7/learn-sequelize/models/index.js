96d36fc9e0ae56b0db02d4d9153c8447b4793018
yunheur <yunheur@gmail.com>
2019-04-01 00:26:24 +0900

    set sequelize models

--- ch7/learn-sequelize/models/index.js
+++ ch7/learn-sequelize/models/index.js
@@ -10,4 +10,10 @@ const sequelize = new Sequelize(config.database, config.username, config.passwor
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
+db.User = require('./user')(sequelize, Sequelize);
+db.Comment = require('./comment')(sequelize, Sequelize);
+
+db.User.hasMany(db.Comment, { foreignKey: 'commenter', sourceKey: 'id' });
+db.Comment.belongsTo(db.User, { foreignKey: 'commenter', targetKey: 'id' });
+
 module.exports = db;
