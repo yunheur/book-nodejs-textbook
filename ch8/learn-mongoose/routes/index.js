ea39dfc827eb0ec16862261eb9b1958fbb8fb579
yunheur <yunheur@gmail.com>
2019-04-04 23:46:14 +0900

    add route (users, comments)

--- ch8/learn-mongoose/routes/index.js
+++ ch8/learn-mongoose/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var User = require('../schemas/user');
+
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  User.find({})
+    .then((users) => {
+      res.render('mongoose', { users });
+    })
+    .catch((err) => {
+      console.error(err);
+      next(err);
+    });
 });
 
 module.exports = router;
