95d4798eae3eb1fa4938ef2c4dd736c0ebdb3076
yunheur <yunheur@gmail.com>
2019-04-05 23:48:05 +0900

    add dotenv

--- ch9/nodebird/app.js
+++ ch9/nodebird/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const path = require('path');
 const session = require('express-session');
 const flash = require('connect-flash');
+require('dotenv').config();
 
 const pageRouter = require('./routes/page');
 
@@ -17,11 +18,11 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('nodebirdsecret'));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
   resave: false,
   saveUninitialized: false,
-  secret: 'nodebirdsecret',
+  secret: process.env.COOKIE_SECRET,
   cookie: {
     httpOnly: true,
     secure: false,
