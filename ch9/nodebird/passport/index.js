55dbb894dad747ab02bb3eaf9b9a8dc487cdec86
yunheur <yunheur@gmail.com>
2019-04-07 15:18:01 +0900

    write search hashtag and follow

--- ch9/nodebird/passport/index.js
+++ ch9/nodebird/passport/index.js
@@ -8,7 +8,18 @@ module.exports = (passport) => {
   });
 
   passport.deserializeUser((id, done) => {
-    User.findOne({ where: { id } })
+    User.findOne({
+      where: { id },
+      include: [{
+        model: User,
+        attributes: ['id', 'nick'],
+        as: 'Followers',
+      }, {
+        model: User,
+        attributes: ['id', 'nick'],
+        as: 'Followings',
+      }],
+    })
       .then(user => done(null, user))
       .catch(err => done(err));
   });
--- ch9/nodebird/routes/post.js
+++ ch9/nodebird/routes/post.js
@@ -55,4 +55,26 @@ router.post('/', isLoggedIn, upload2.none(), async (req, res, next) => {
   }
 });
 
+router.get('/hashtag', async (req, res, next) => {
+  const query = req.query.hashtag;
+  if (!query) {
+    return res.redirect('/');
+  }
+  try {
+    const hashtag = await Hashtag.findOne({ where: { title: query } });
+    let posts = [];
+    if (hashtag) {
+      posts = await hashtag.getPosts({ include: [{ model: User }] });
+    }
+    return res.render('main', {
+      title: `${query} | NodeBird`,
+      user: req.user,
+      twits: posts,
+    });
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+});
+
 module.exports = router;
