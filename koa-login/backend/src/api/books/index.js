903686c1776534fb62245af4107d7814069fc4a4
yunheur <yunheur@gmail.com>
2019-05-17 00:07:28 +0900

    - mongoose   - 스키마 작성 - controller   - 데이터 생성, 조회, 삭제, 수정, 요청 검증 - modules   - dotenv   - bodyparser   - nodemon   - joi

--- koa-login/backend/src/api/books/index.js
+++ koa-login/backend/src/api/books/index.js
@@ -4,9 +4,10 @@ const books = new Router();
 const booksCtrl = require('./books.controller');
 
 books.get('/', booksCtrl.list);
+books.get('/:id', booksCtrl.get);
 books.post('/', booksCtrl.create);
-books.delete('/', booksCtrl.delete);
-books.put('/', booksCtrl.replace);
-books.patch('/', booksCtrl.update);
+books.delete('/:id', booksCtrl.delete);
+books.put('/:id', booksCtrl.replace);
+books.patch('/:id', booksCtrl.update);
 
 module.exports = books;
--- koa-login/backend/src/index.js
+++ koa-login/backend/src/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config(); // .env 파일에서 환경변수 불러오기
+
 const Koa = require('koa');
 const Router = require('koa-router');
 
@@ -5,10 +7,28 @@ const app = new Koa();
 const router = new Router();
 const api = require('./api');
 
-router.use('/api', api.routes()); // api 라우트를 /api 경로 하위 라우트로 설정
+const mongoose = require('mongoose');
+const bodyParser = require('koa-bodyparser');
 
+mongoose.Promise = global.Promise; // Node 의 네이티브 Promise 사용
+// mongodb 연결
+mongoose.connect(process.env.MONGO_URI, {
+  dbName: process.env.MONGO_DB
+}).then(
+  (response) => {
+    console.log('Successfully connected to mongodb');
+  }
+).catch(e => {
+  console.error(e);
+});
+
+const port = process.env.PORT || 4000; // PORT 값이 설정되어있지 않다면 4000 을 사용합니다.
+
+app.use(bodyParser()); // 바디파서 적용, 라우터 적용코드보다 상단에 있어야합니다.
+
+router.use('/api', api.routes()); // api 라우트를 /api 경로 하위 라우트로 설정
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-  console.log('koa-login server is listening to port 4000');
+app.listen(port, () => {
+  console.log('koa login server is listening to port ' + port);
 });
