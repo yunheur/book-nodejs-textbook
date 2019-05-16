083dc7140293dcca8c9d9c8924043c8fece7d0d6
yunheur <yunheur@gmail.com>
2019-05-16 22:15:55 +0900

    #1 koa-login - 개발 환경 구축, 백앤드 서버 구조 잡기

---
+++ koa-login/backend/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: "standard",
+  rules: {
+    indent: ["error", 2],
+    semi: ["error", "always"],
+    "no-trailing-spaces": 0,
+    "keyword-spacing": 0,
+    "no-unused-vars": 1,
+    "no-multiple-empty-lines": 0,
+    "space-before-function-paren": 0,
+    "eol-last": 0
+  }
+};
---
+++ koa-login/backend/src/index.js
@@ -0,0 +1,14 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const app = new Koa();
+const router = new Router();
+const api = require('./api');
+
+router.use('/api', api.routes()); // api 라우트를 /api 경로 하위 라우트로 설정
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(4000, () => {
+  console.log('koa-login server is listening to port 4000');
+});
