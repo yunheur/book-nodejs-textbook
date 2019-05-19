086f196317601916a6a407da1347b3a13a65cb31
yunheur <yunheur@gmail.com>
2019-05-19 21:52:08 +0900

    #1 koa-login - 리덕스 설정

--- koa-login/frontend/src/index.js
+++ koa-login/frontend/src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Root from './Root';
 import * as serviceWorker from './serviceWorker';
+import configureStore from 'redux/configureStore';
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const store = configureStore();
+
+ReactDOM.render(<Root store={store}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
