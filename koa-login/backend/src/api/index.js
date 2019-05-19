aa0fbb95ae19d44242aa0ee42ffaa1722d14bb25
yunheur <yunheur@gmail.com>
2019-05-19 18:34:46 +0900

    #1 koa-login - 로컬 인증 API 컨트롤러 및 라우터 설정

--- koa-login/backend/src/api/index.js
+++ koa-login/backend/src/api/index.js
@@ -2,7 +2,9 @@ const Router = require('koa-router');
 
 const api = new Router();
 const books = require('./books');
+const auth = require('./auth');
 
 api.use('/books', books.routes());
+api.use('/auth', auth.routes());
 
 module.exports = api;
