91a9ac8b54cb3ab99479125623b25821bb50933b
yunheur <yunheur@gmail.com>
2019-05-20 23:33:52 +0900

    #1 koa-login - API 함수 준비 및 redux-pender 적용

--- koa-login/frontend/src/redux/configureStore.js
+++ koa-login/frontend/src/redux/configureStore.js
@@ -1,19 +1,24 @@
-import { createStore } from 'redux';
-
+import { createStore, applyMiddleware, compose } from 'redux';
+import penderMiddleware from 'redux-pender';
 import modules from './modules';
+
+const isDevelopment = process.env.NODE_ENV === 'development'; // 환경이 개발모드인지 확인합니다
+const composeEnhancers = isDevelopment ? (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose) : compose;
 
 // Todo: 미들웨어, react-hot-loader 적용
 const configureStore = (initialState) => {
-    const store = createStore(modules, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-    
+    const store = createStore(modules, initialState, composeEnhancers(
+        applyMiddleware(penderMiddleware())
+    ));
+
     // hot-reloading 를 위한 코드
     if(module.hot) {
-      module.hot.accept('./modules', () => {
-          const nextRootReducer = require('./modules').default;
-          store.replaceReducer(nextRootReducer);
-      });
+        module.hot.accept('./modules', () => {
+            const nextRootReducer = require('./modules').default;
+            store.replaceReducer(nextRootReducer);
+        });
     }
-    
+
     return store;
 }
 
