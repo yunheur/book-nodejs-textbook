d636286650934cf9d94266a5f454add2c1c73353
yunheur <yunheur@gmail.com>
2019-05-20 22:01:55 +0900

    #1 koa-login - 헤더 숨기기

--- koa-login/frontend/src/containers/Base/HeaderContainer.js
+++ koa-login/frontend/src/containers/Base/HeaderContainer.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
-import Header from 'components/Base/Header';
+import Header, { LoginButton } from 'components/Base/Header';
+import { connect } from 'react-redux';
 
 class HeaderContainer extends Component {
     render() {
+        const { visible } = this.props;
+        if(!visible) return null;
+
         return (
             <Header>
-                Hi
+                <LoginButton/>
             </Header>
         );
     }
 }
 
-export default HeaderContainer;
+export default connect(
+    (state) => ({
+        visible: state.base.getIn(['header', 'visible'])
+    }),
+    (dispatch) => ({
+
+    })
+)(HeaderContainer);
