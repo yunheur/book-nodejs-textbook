c7880fdc91696c8f6595a56559561cc593c8450d
yunheur <yunheur@gmail.com>
2019-05-20 22:36:14 +0900

    #1 koa-login - 로그인 틀 만들기

---
+++ koa-login/frontend/src/components/Auth/index.js
@@ -0,0 +1,4 @@
+export { default as AuthWrapper } from './AuthWrapper';
+export { default as AuthContent } from './AuthContent';
+export { default as Login } from './Login';
+export { default as Register } from './Register';
--- koa-login/frontend/src/pages/Auth.js
+++ koa-login/frontend/src/pages/Auth.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as baseActions from 'redux/modules/base';
+import { AuthWrapper } from 'components/Auth';
+import { Route } from 'react-router-dom';
+import { Login, Register } from 'components/Auth';
 
 class Auth extends Component {
     // 페이지에 진입 할 때 헤더를 비활성화
@@ -16,9 +19,10 @@ class Auth extends Component {
 
     render() {
         return (
-            <div>
-                Auth
-            </div>
+            <AuthWrapper>
+                <Route path="/auth/login" component={Login}/>
+                <Route path="/auth/register" component={Register}/>
+            </AuthWrapper>
         );
     }
 }
