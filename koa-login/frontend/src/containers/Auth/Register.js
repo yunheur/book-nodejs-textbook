774c96f873ffd0ead0c67ee3d1dba999912e5010
yunheur <yunheur@gmail.com>
2019-05-20 23:47:41 +0900

    #1 koa-login - 문자열 검증하기, 오류 보여주기

--- koa-login/frontend/src/containers/Auth/Register.js
+++ koa-login/frontend/src/containers/Auth/Register.js
@@ -1,16 +1,81 @@
 import React, { Component } from 'react';
-import { AuthContent, InputWithLabel, AuthButton, RightAlignedLink } from 'components/Auth';
+import { AuthContent, InputWithLabel, AuthButton, RightAlignedLink, AuthError } from 'components/Auth';
 import { connect } from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as authActions from 'redux/modules/auth';
-
+import {isEmail, isLength, isAlphanumeric} from 'validator';
+import debounce from 'lodash/debounce';
 
 class Register extends Component {
 
-    componentWillUnmount() {
+    setError = (message) => {
         const { AuthActions } = this.props;
-        AuthActions.initializeForm('register')
+        AuthActions.setError({
+            form: 'register',
+            message
+        });
     }
+
+    validate = {
+        email: (value) => {
+            if(!isEmail(value)) {
+                this.setError('잘못된 이메일 형식 입니다.');
+                return false;
+            }
+            return true;
+        },
+        username: (value) => {
+            if(!isAlphanumeric(value) || !isLength(value, { min:4, max: 15 })) {
+                this.setError('아이디는 4~15 글자의 알파벳 혹은 숫자로 이뤄져야 합니다.');
+                return false;
+            }
+            return true;
+        },
+        password: (value) => {
+            if(!isLength(value, { min: 6 })) {
+                this.setError('비밀번호를 6자 이상 입력하세요.');
+                return false;
+            }
+            this.setError(null); // 이메일과 아이디는 에러 null 처리를 중복확인 부분에서 하게 됩니다
+            return true;
+        },
+        passwordConfirm: (value) => {
+            if(this.props.form.get('password') !== value) {
+                this.setError('비밀번호확인이 일치하지 않습니다.');
+                return false;
+            }
+            this.setError(null); 
+            return true;
+        }
+    }
+
+    checkEmailExists = debounce(async (email) => {
+        const { AuthActions } = this.props;
+        try {
+            await AuthActions.checkEmailExists(email);
+            if(this.props.exists.get('email')) {
+                this.setError('이미 존재하는 이메일입니다.');
+            } else {
+                this.setError(null);
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }, 300)
+
+    checkUsernameExists = debounce(async (username) => {
+        const { AuthActions } = this.props;
+        try {
+            await AuthActions.checkUsernameExists(username);
+            if(this.props.exists.get('username')) {
+                this.setError('이미 존재하는 아이디입니다.');
+            } else {
+                this.setError(null);
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }, 300)
 
     handleChange = (e) => {
         const { AuthActions } = this.props;
@@ -21,9 +86,18 @@ class Register extends Component {
             value,
             form: 'register'
         });
+
+        // 검증작업 진행
+        const validation = this.validate[name](value);
+        if(name.indexOf('password') > -1 || !validation) return; // 비밀번호 검증이거나, 검증 실패하면 여기서 마침
+
+        // TODO: 이메일, 아이디 중복 확인
+        const check = name === 'email' ? this.checkEmailExists : this.checkUsernameExists; // name 에 따라 이메일체크할지 아이디 체크 할지 결정
+        check(value);
     }
 
     render() {
+        const { error } = this.props;
         const { email, username, password, passwordConfirm } = this.props.form.toJS();
         const { handleChange } = this;
 
@@ -59,6 +133,9 @@ class Register extends Component {
                     value={passwordConfirm}
                     onChange={handleChange}
                 />
+                {
+                    error && <AuthError>{error}</AuthError>
+                }
                 <AuthButton>회원가입</AuthButton>
                 <RightAlignedLink to="/auth/login">로그인</RightAlignedLink>
             </AuthContent>
@@ -68,7 +145,9 @@ class Register extends Component {
 
 export default connect(
     (state) => ({
-        form: state.auth.getIn(['register', 'form'])
+        form: state.auth.getIn(['register', 'form']),
+        error: state.auth.getIn(['register', 'error']),
+        exists: state.auth.getIn(['register', 'exists'])
     }),
     (dispatch) => ({
         AuthActions: bindActionCreators(authActions, dispatch)
