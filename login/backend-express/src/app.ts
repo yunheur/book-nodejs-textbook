de31aa73b2c43f1dc74b37aa8a880c011dc90498
yunheur <yunheur@gmail.com>
2019-06-04 23:30:31 +0900

    #7 login 프로젝트에서 express backend 서버 작성 - app.ts 클래스 문법으로 수정

--- login/backend-express/src/app.ts
+++ login/backend-express/src/app.ts
@@ -1,14 +1,32 @@
 require('dotenv').config(); // .env 파일에서 환경변수 불러오기
-import express from 'express';
+import express, { Express } from 'express';
 
-const app = express();
+class App {
+  private app: Express;
 
-app.set('port', process.env.PORT || 4000); // PORT 값이 설정되어있지 않다면 4000 을 사용합니다.
+  constructor() {
+    this.app = express();
+    this.app.get('/', (req: express.Request, res: express.Response) => {
+      res.send('Hello World!');
+    });
+    this.setApp();
+    this.middlewares();
+  }
 
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('Hello World!');
-});
+  public start = (): void => {
+    this.app.listen(this.app.get('port'), () => {
+      console.log(this.app.get('port'), '번 포트에서 대기중');
+    });
+  };
 
-app.listen(app.get('port'), () => {
-  console.log(app.get('port'), '번 포트에서 대기중');
-});
+  private setApp = (): void => {
+    console.log(process.env.PORT);
+    this.app.set('port', process.env.PORT || 4000); // PORT 값이 설정되어있지 않다면 4000 을 사용합니다.
+  };
+
+  private middlewares = (): void => {
+    console.log('middleware');
+  };
+}
+
+export default new App().start();
