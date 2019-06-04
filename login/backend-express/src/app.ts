388f4f8584c7dde6bcefeb1294ee9cc5ff0a67a1
yunheur <yunheur@gmail.com>
2019-06-04 22:49:22 +0900

    #7 login 프로젝트에서 express backend 서버 작성 - script 작성 및 .env 파일 생성

--- login/backend-express/src/app.ts
+++ login/backend-express/src/app.ts
@@ -1,10 +1,14 @@
+require('dotenv').config(); // .env 파일에서 환경변수 불러오기
 import express from 'express';
+
 const app = express();
+
+app.set('port', process.env.PORT || 4000); // PORT 값이 설정되어있지 않다면 4000 을 사용합니다.
 
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기중');
 });
