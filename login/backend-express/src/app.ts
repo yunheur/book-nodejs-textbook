85682600d5c2eef32574b96f2c50d6d3a836be59
yunheur <yunheur@gmail.com>
2019-06-07 13:53:10 +0900

    #7 login 프로젝트에서 express backend 서버 작성 - morgan (콘솔에 로그 남기기) 추가

--- login/backend-express/src/app.ts
+++ login/backend-express/src/app.ts
@@ -1,6 +1,7 @@
 require('dotenv').config(); // .env 파일에서 환경변수 불러오기
 
 import express, { Express } from 'express';
+import morgan = require('morgan');
 import routes from './routes';
 import { sequelize } from './sequelize';
 
@@ -9,7 +10,7 @@ class App {
 
   constructor() {
     this.app = express();
-    this.app.set('port', process.env.PORT || 4000); // PORT 값이 설정되어있지 않다면 4000 을 사용합니다.
+    this.serverSetting();
     this.middlewares();
   }
 
@@ -21,7 +22,16 @@ class App {
     });
   };
 
+  private serverSetting = (): void => {
+    this.app.set('port', process.env.PORT || 4000); // PORT 값이 설정되어있지 않다면 4000 을 사용합니다.
+  };
+
   private middlewares = (): void => {
+    if (process.env.NODE_ENV === 'production') {
+      this.app.use(morgan('combined'));
+    } else {
+      this.app.use(morgan('dev'));
+    }
     this.app.use(express.json());
     this.app.use(express.urlencoded({ extended: false }));
     this.app.use('/api', routes);
